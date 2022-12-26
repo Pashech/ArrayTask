a8e23adcec8abd6857f017c510e5e6c3d2eb8cee
pashech <pashech555@gmail.com>
2022-12-26 23:48:08 +0300

    add test for resultArray_2

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,24 +2,33 @@ package org.example;
 
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
 
 public class AppTest {
 
+    private int[] actual;
+    private int[] actual_2;
+
+    @BeforeEach
+    public void prepareData(){
+        actual = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15};
+        actual_2 = new int[]{0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14};
+    }
 
     @Test
     void resultArrayStream() {
-        int[] actual = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15};
-        int[] actual2 = {0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14};
         int[] expectedResult = App.resultArrayStream(actual);
-        int[] expectedResult2 = App.resultArrayStream(actual2);
-        Assertions.assertTrue(Arrays.equals(expectedResult, new int[]{10, -65}));
-        Assertions.assertTrue(Arrays.equals(expectedResult2, new int[]{8, -50}));
+        int[] expectedResult2 = App.resultArrayStream(actual_2);
+        Assertions.assertArrayEquals(expectedResult, new int[]{10, -65});
+        Assertions.assertArrayEquals(expectedResult2, new int[]{8, -50});
     }
 
     @Test
     void resultArray() {
+        int[] expectedResult = App.resultArrayStream(actual);
+        int[] expectedResult2 = App.resultArrayStream(actual_2);
+        Assertions.assertArrayEquals(expectedResult, new int[]{10, -65});
+        Assertions.assertArrayEquals(expectedResult2, new int[]{8, -50});
     }
 }
