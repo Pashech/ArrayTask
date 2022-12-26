ecf0b2991993e980fc3e49d313c0e20185d790f3
pashech <pashech555@gmail.com>
2022-12-26 23:29:33 +0300

    add test for resultArrayStream

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,18 +1,25 @@
 package org.example;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertTrue;
-
-
-
+import java.util.Arrays;
 
 public class AppTest {
 
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void resultArrayStream() {
+        int[] actual = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15};
+        int[] actual2 = {0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14};
+        int[] expectedResult = App.resultArrayStream(actual);
+        int[] expectedResult2 = App.resultArrayStream(actual2);
+        Assertions.assertTrue(Arrays.equals(expectedResult, new int[]{10, -65}));
+        Assertions.assertTrue(Arrays.equals(expectedResult2, new int[]{8, -50}));
+    }
+
+    @Test
+    void resultArray() {
     }
 }
