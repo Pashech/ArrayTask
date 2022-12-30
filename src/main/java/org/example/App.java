42773df49e3c49aec6a5a06108c9c6cde76d0ccd
pashech <pashech555@gmail.com>
2022-12-30 17:18:36 +0300

    add method reduce

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,6 +12,8 @@ public class App {
         int[] newArray2 = resultArray(array2);
         int[] newArrayStream = resultArrayStream(array);
         int[] newArrayStream2 = resultArrayStream(array2);
+        int[] newArrayStreamWithReduce = resultArrayWithReduce(array);
+        int[] newArrayStreamWithReduce2 = resultArrayWithReduce(array2);
 
         Arrays.stream(newArray)
                 .forEach(System.out::println);
@@ -20,6 +22,10 @@ public class App {
         Arrays.stream(newArrayStream)
                 .forEach(System.out::println);
         Arrays.stream(newArrayStream2)
+                .forEach(System.out::println);
+        Arrays.stream(newArrayStreamWithReduce)
+                .forEach(System.out::println);
+        Arrays.stream(newArrayStreamWithReduce2)
                 .forEach(System.out::println);
 
     }
@@ -32,11 +38,11 @@ public class App {
         return Arrays.stream(array)
                 .collect(() -> new int[2],
                         (arr, i) -> {
-                    if(i > 0){
-                        arr[0]++;
-                    }else {
-                        arr[1] += i;
-                    }
+                            if (i > 0) {
+                                arr[0]++;
+                            } else {
+                                arr[1] += i;
+                            }
                         },
                         (count, sum) -> {
                             count[0] = sum[0];
@@ -59,7 +65,29 @@ public class App {
                 sum += j;
             }
         }
-
         return new int[]{count, sum};
+    }
+
+    public static int[] resultArrayWithReduce(int[] array) {
+
+        if ((array == null) || (array.length == 0)) {
+            return new int[]{};
+        }
+        final int[] mass = new int[2];
+        final int[] count = new int[1];
+        final int[] sum = new int[1];
+        Arrays.stream(array).reduce(0, (acc, elem) -> {
+            if (elem > 0) {
+                mass[0]++;
+            } else {
+                acc += elem;
+                mass[1] = acc;
+            }
+
+            count[0] = mass[0];
+            sum[0] = mass[1];
+            return acc;
+        });
+        return new int[]{count[0], sum[0]};
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -9,9 +9,17 @@ public class AppTest {
     private int[] actual_2;
 
     @BeforeEach
-    public void prepareData(){
+    public void prepareData() {
         actual = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15};
         actual_2 = new int[]{0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14};
+    }
+
+    @Test
+    void resultArrayStreamWithReduce() {
+        int[] expectedResult = App.resultArrayWithReduce(actual);
+        int[] expectedResult2 = App.resultArrayWithReduce(actual_2);
+        Assertions.assertArrayEquals(expectedResult, new int[]{10, -65});
+        Assertions.assertArrayEquals(expectedResult2, new int[]{8, -50});
     }
 
     @Test
